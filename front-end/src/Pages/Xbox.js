2cd6e7df96010e861f633ff2ebff7aaeeb58c8aa
Berlin Rivas <berlinrivas@Berlins-MacBook-Air.local>
2023-08-13 17:38:26 -0400

    latest update

--- front-end/src/Pages/Xbox.js
+++
@@ -1,8 +0,0 @@
-export default function Xbox() {
-    return (
-    <>
-    <iframe className="xbox" width="650" height="395" src="https://www.youtube.com/embed/qGtYn7DCIYo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-    <h2 className="title-microsoft">Xbox Games Showcase + Starfield</h2>
-    </>
-    )
-}
